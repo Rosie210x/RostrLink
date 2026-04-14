aacfbf7e0d0038b717f2a566a189b48599db9096
Dang Hong Minh Tam <dhmtam153@gmail.com>
2026-04-14 13:09:21 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RostrLink
+
---
+++ src/main/java/com/rostrlink/App.java
@@ -0,0 +1,13 @@
+package com.rostrlink;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
